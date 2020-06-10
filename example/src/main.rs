cdafe03ec553bdbe15bde89f4e0aee954f677912
sunjian126 <sunjian11@jd.com>
2020-06-10 12:08:42 +0800

    up

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,52 +4,51 @@ use log::{debug, info};
 use raft4rs::{entity::Config, error::*, server::Server, state_machine::*};
 use std::sync::Arc;
 
-fn main() {
-    task::block_on(async {
-        env_logger::init_from_env(
-            env_logger::Env::default()
-                .filter_or("MY_LOG_LEVEL", "debug")
-                .write_style_or("auto", "always"),
-        );
+#[async_std::main]
+async fn main() {
+    env_logger::init_from_env(
+        env_logger::Env::default()
+            .filter_or("MY_LOG_LEVEL", "debug")
+            .write_style_or("auto", "always"),
+    );
 
-        debug!("start............");
+    debug!("start............");
 
-        let server1 = Arc::new(Server::new(make_config(1), make_resolver())).start();
-        let server2 = Arc::new(Server::new(make_config(2), make_resolver())).start();
-        let server3 = Arc::new(Server::new(make_config(3), make_resolver())).start();
+    let server1 = Arc::new(Server::new(make_config(1), make_resolver())).start();
+    let server2 = Arc::new(Server::new(make_config(2), make_resolver())).start();
+    let server3 = Arc::new(Server::new(make_config(3), make_resolver())).start();
 
-        let replicas = &vec![1, 2, 3];
+    let replicas = &vec![1, 2, 3];
 
-        let raft1 = server1
-            .create_raft(1, 1, &replicas, SM { id: 1 })
+    let raft1 = server1
+        .create_raft(1, 1, &replicas, SM { id: 1 })
+        .await
+        .unwrap();
+    let _raft2 = server2
+        .create_raft(1, 0, &replicas, SM { id: 2 })
+        .await
+        .unwrap();
+    let _raft3 = server3
+        .create_raft(1, 0, &replicas, SM { id: 3 })
+        .await
+        .unwrap();
+
+    while !raft1.is_leader().await {
+        raft1.try_to_leader().await.unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        info!("wait raft1 to leader times");
+    }
+    for i in 0..1000000u32 {
+        if let Err(e) = raft1
+            .submit(unsafe { format!("commit: {}", i + 1).as_mut_vec().clone() })
             .await
-            .unwrap();
-        let _raft2 = server2
-            .create_raft(1, 0, &replicas, SM { id: 2 })
-            .await
-            .unwrap();
-        let _raft3 = server3
-            .create_raft(1, 0, &replicas, SM { id: 3 })
-            .await
-            .unwrap();
-
-        while !raft1.is_leader().await {
-            raft1.try_to_leader().await.unwrap();
+        {
+            println!("{}", e);
             std::thread::sleep(std::time::Duration::from_secs(1));
-            info!("wait raft1 to leader times");
         }
-        for i in 0..1000000u32 {
-            if let Err(e) = raft1
-                .submit(unsafe { format!("commit: {}", i + 1).as_mut_vec().clone() })
-                .await
-            {
-                println!("{}", e);
-                std::thread::sleep(std::time::Duration::from_secs(1));
-            }
-        }
+    }
 
-        std::thread::sleep(std::time::Duration::from_secs(10000));
-    });
+    std::thread::sleep(std::time::Duration::from_secs(10000));
 }
 
 struct SM {
